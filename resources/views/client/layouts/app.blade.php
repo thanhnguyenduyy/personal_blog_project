7036a8a24492dc75e3942c2523c07ad3ec911298
thanhnguyenduyy <thanhnguyenduyy@gmail.com>
2026-03-11 23:51:03 +0700

    upd src

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'IT & Photography Portfolio')</title>
-    <meta name="description" content="@yield('description', 'Portfolio của Nguyễn Duy Thanh - Developer và Photographer')">
+    <meta name="description" content="@yield('description', 'Portfolio của Your Name - Developer và Photographer')">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
